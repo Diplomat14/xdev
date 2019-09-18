deae6e9db33166f4c61ffbcf12834f5c7a4029bf
Aleksey Denysyuk <diplomt@gmailcom>
2019-09-18 20:03:29 +0300

    Parent issue added to tree node. Function for removal of root node added.

--- xdev/types/complex/tree.py
+++ xdev/types/complex/tree.py
@@ -12,6 +12,11 @@ class tree_type(object):
         if rootnode not in self.__roots:
             self.__roots.append(rootnode)
 
+    def remove_root_node(self,rootnode):
+        assert isinstance(rootnode, tree_node_type), "Tree node is of wrong type"
+        if rootnode in self.__roots:
+            self.__roots.remove(rootnode)
+
     # Treats root node of provided subtree as one of root nddes for this tree
     def addRootSubtree(self,subtree):
         assert isinstance(subtree, tree_type), "Tree is of wrong type"
@@ -48,7 +53,12 @@ class tree_node_type(object):
 
     def __init__(self, data):
         self.__data = data
+        self.__parent = None
         self.__children = []
+
+    def __set_parent(self,parent):
+        assert isinstance(parent, tree_node_type), "Tree node is of wrong type"
+        self.__parent = parent
 
     def add_children(self,children):
         for ch in children:
@@ -57,6 +67,7 @@ class tree_node_type(object):
     def add_child(self,childnode):
         assert isinstance(childnode, tree_node_type), "Tree node is of wrong type"
         self.__children.append(childnode)
+        childnode.__set_parent(self)
 
     def has_direct_child(self, childnode):
         return True if childnode in self.__children else False
@@ -67,6 +78,10 @@ class tree_node_type(object):
     @property
     def data(self):
         return self.__data
+
+    @property
+    def parent(self):
+        return self.__parent
 
     @property
     def children(self):
